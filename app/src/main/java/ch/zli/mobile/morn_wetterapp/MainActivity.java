053ee11a901529dcefa8462fabbbfd4d4a741dee
filipmornshuel <slavkovicf@bzz.ch>
2023-07-06 10:37:42 +0200

    Initial commit

---
+++ app/src/main/java/ch/zli/mobile/morn_wetterapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ch.zli.mobile.morn_wetterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
